9d248e012072720f6400d3e4b2eb9ec0e04faff0
Sadia_S <37849950+Sadia2018@users.noreply.github.com>
2021-12-12 15:58:19 -0800

    front methods linked lists

---
+++ fronts.js
@@ -0,0 +1,23 @@
+class Node {
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+addFront(val){
+    let new_node = new Node(val);
+    if(!this.head) {
+        this.head = new_node;
+        return this;
+    
+    }
+    new_node.next = this.head;
+    this.head = new_node;
+    return this;
+}
+}
