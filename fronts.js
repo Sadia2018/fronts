976484557c7066d3d4eca04dc922772170392acb
Sadia_S <37849950+Sadia2018@users.noreply.github.com>
2021-12-12 16:10:59 -0800

    more methods

--- fronts.js
+++ fronts.js
@@ -1,23 +1,40 @@
-class Node {
-    constructor(data){
-        this.data = data;
-        this.next = null;
-    }
-}
-class LinkedList {
-    constructor() {
-        this.head = null;
-    }
+//Write a method that accepts a value and create a new node, assign it to the list head, and return a pointer to the new head node.
 
-addFront(val){
-    let new_node = new Node(val);
-    if(!this.head) {
-        this.head = new_node;
-        return this;
-    
+class SLL {
+      addFront(value) {
+        node = new Node()
+        if (this.head === null) {
+            this.head = node;
+        } else {
+
+            var current = this.head;
+            while (current.next !== null) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+
     }
-    new_node.next = this.head;
-    this.head = new_node;
-    return this;
+    
 }
+//Write a method to remove the head node and return the new list head node. If the list is empty, return null.
+
+class SLL {
+    removeFront() {
+        if ((this.head === null)) {
+            return null
+        } else {
+            this.head = this.head.next;
+            return this
+        }
+    }
 }
+
+//Write a method to return the value (not the node) at the head of the list. If the list is empty, return null.
+class SLL {
+    
+}
+
+ 
+
+ 
